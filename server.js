af34970a7b4e1852524ec33dc51f54fd4b7519c6
sugerhater <kehong0926@gmail.com>
2021-02-17 21:38:01 -0500

    basic set up

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const mongoose = require("mongoose");
+
+const PORT = process.env.PORT ||3000;
+
+const db = require("./models");
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/populate", { useNewUrlParser: true });
